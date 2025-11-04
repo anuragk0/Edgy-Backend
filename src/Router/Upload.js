c56056514f2ae96c29d19afd044d88ea0abb1279
anuragk0 <anuragk7028@gmail.com>
2025-11-04 10:48:24 +0530

    minor changes

--- src/Router/Upload.js
+++ src/Router/Upload.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const { generateFlashCards, answerQuestion } = require('../Controllers/Upload');
 const uploadWithErrorHandler = require('../../middleware/uploadMiddleware');
 const geminiLimiter = require('../../middleware/geminiLimiter');
+const {isAuthenticated} = require('../Controllers/User')
 
-router.post('/uploadFile', geminiLimiter, uploadWithErrorHandler, generateFlashCards);
-router.post('/answer', geminiLimiter, answerQuestion);
+router.post('/uploadFile', isAuthenticated, geminiLimiter, uploadWithErrorHandler, generateFlashCards);
+router.post('/answer', isAuthenticated, geminiLimiter, answerQuestion);
 
 module.exports = router;
